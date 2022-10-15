833ea22ca287b4e1b3662e0ce710b9e8dd8ab0c1
Alexander Bojantchev <alexanderbojantchev@Alexanders-MacBook-Pro.local>
2022-10-15 18:17:21 +0300

    first commit

---
+++ test1.js
@@ -0,0 +1,13 @@
+let ar = [1,5,2,6,8,0,]
+
+/*bigger = ar.filter((val) => {
+  return val > 5;
+});
+
+console.log(bigger);*/
+
+console.log(ar.filter(v => v > 5));
+console.log(ar.indexOf(6))
+let str1 = "my dog is the best dog in the world"
+console.log(str1.split(' '))
+console.log(str1.split(' ').join('__'))
